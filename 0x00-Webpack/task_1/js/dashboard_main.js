1d3db34e13e0eeebd26948619e36be5a9609326d
Valentine Maduagwu <vchidubem9@gmail.com>
2024-08-01 20:37:32 +0100

    Learning how to use Webpack with a config file

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -6,16 +6,16 @@ $('body').append("<p>Dashboard data for the students</p>");
 $('body').append("<button>Click here to get started</button>");
 $('body').append("<p id='count'></p>");
 $('body').append("<p>Copyright - Holberton School</p>");
-$('button').on("click", _.debounce(updateCounter));
+$('button').on("click", _.debounce(updateCounter, 500, { leading: true, trailing: false }));
 
 function updateCounter(){
 	let count = $("#count").val()
-	if (!count) {
+	if (count) {
+		count = +count + 1
+		$("#count").text(`${count} clicks on the button`)
+
+	} else {
 		$("#count").val(1)
 		$("#count").text(`${1} clicks on the button`)
-	} else {
-		count = +count + 1
-		$("#count").val(count)
-		$("#count").text(`${count} clicks on the button`)
 	}
 }
